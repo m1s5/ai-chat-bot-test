9dae06cbb7e8ac1b04bffdb1d32880f2aa664dba
m1s5 <m1s5.trash@yandex.ru>
2023-12-09 20:43:34 +0300

    fox build

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  images: { unoptimized: true },
+  experimental: {},
+  basePath: "/ai-chat-bot-test",
   reactStrictMode: true,
 
   /**
